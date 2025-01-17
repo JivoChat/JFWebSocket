97ae123a9e61554e7830cd8f8b2b3eb5387d4fb1
Stan Potemkin <stan@potemkin.su>
2025-01-17 08:51:06 +0300

    Fix the Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,19 @@
+// swift-tools-version:5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-    name: "JFWebSocket"
+    name: "JFWebSocket",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "JFWebSocket",
+            targets: ["JFWebSocket"])
+    ],
+    targets: [
+        .target(
+            name: "JFWebSocket",
+            path: "Source")
+    ]
 )
